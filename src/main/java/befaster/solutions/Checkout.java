e9ddb1a03d7c1cf06f8c7fd12c6dd7d7e5a178ce
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-30 15:12:23 +0000

    Sat Dec 30 15:12:23 GMT 2017

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -3,7 +3,8 @@ package befaster.solutions;
 import befaster.runner.SolutionNotImplementedException;
 
 public class Checkout {
+
     public static Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        if()
     }
 }
